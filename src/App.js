0481e93d019131b14183d4b08bf528ae97e17e04
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2021-01-31 19:01:20 +0100

    added skills section

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import About from "./components/About";
+import Skills from "./components/Skills";
 function App() {
   return (
     <div className="container">
       <Header />
       <Navbar />
       <About />
+      <Skills />
     </div>
   );
 }
---
+++ src/components/Skills.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles/Skills.css";
+
+export default function Skills() {
+  return (
+    <div className="skills">
+      <h2 className="skillsTitle">SKILLS</h2>
+      <div className="skillsRectangle"></div>
+    </div>
+  );
+}
