d5f337a54c610fa112d6cec8aae4dc813355fb2f
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2021-02-10 17:09:17 +0100

    added icons for About

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,40 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPalette } from "@fortawesome/free-solid-svg-icons";
+import { faPeopleCarry } from "@fortawesome/free-solid-svg-icons";
+import { faBookReader } from "@fortawesome/free-solid-svg-icons";
 import "../styles/About.css";
 
 export default function About() {
   return (
-    <div name= "content"className="about content">
+    <div name="content" className="about content">
       <h1 className="aboutTitle">ABOUT</h1>
+      <div className="iconContainer">
+        <div className="iconText">
+          <FontAwesomeIcon className="icon Palette" icon={faPalette} />
+          <h4>Creative</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod
+          </p>
+        </div>
+        <div className="iconText">
+          <FontAwesomeIcon className="icon People" icon={faPeopleCarry} />
+          <h4>Team worker</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod
+          </p>
+        </div>
+        <div className="iconText">
+          <FontAwesomeIcon className="icon Book" icon={faBookReader} />
+          <h4>Rapid learner</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod
+          </p>
+        </div>
+      </div>
       <div className="aboutContainer">
         <p className="aboutText">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
@@ -14,11 +44,7 @@ export default function About() {
           hendrerit, vehicula ipsum ac, tristique est. Phasellus tempor enim
           fringilla convallis hendrerit. Cras lorem leo, pretium in fermentum
           ac, ullamcorper eget ipsum. Quisque at maximus est, sollicitudin
-          malesuada ex. Ut hendrerit dignissim tortor sit amet porta.
-          Pellentesque varius imperdiet mattis. Aenean condimentum est luctus
-          nisi tempor luctus. Nunc ultrices luctus nunc eget facilisis. Fusce
-          semper condimentum mi, nec aliquam eros ullamcorper a. Suspendisse
-          porttitor mattis tristique. Sed nec volutpat massa.
+          malesuada ex.
         </p>
         <img className="profilePhoto" src="images/profile.jpg" alt="profile" />
       </div>
