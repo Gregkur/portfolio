2c587e7ff8a090453fd6f567f5a9a8e15e1bdb1b
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2021-02-10 19:54:11 +0100

    added basic skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,10 +2,31 @@ import React from "react";
 import "../styles/Skills.css";
 
 export default function Skills() {
+  const skills = [
+    "HTML",
+    "CSS",
+    "SASS",
+    "Javascript",
+    "Bootstrap",
+    "Ruby",
+    "Rails",
+    "ReactJS",
+    "PostgreSQL",
+    "Figma",
+    "GIT",
+    "Teamwork",
+  ];
+  const mappedSkills = skills.map((skill) => (
+    <div className="singleSkill">{skill}</div>
+  ));
   return (
     <div className="skills">
       <h1 className="skillsTitle">SKILLS</h1>
-      <div className="skillsRectangle"></div>
+      <div className="skillsRectangle">
+        {/* <div className="="> */}
+          {mappedSkills}
+          {/* </div> */}
+      </div>
     </div>
   );
 }
