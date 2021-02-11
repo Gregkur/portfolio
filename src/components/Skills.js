c363fc64e2dd37135442f4e09d610105d30644b3
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2021-02-11 10:53:33 +0100

    animated skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Fade from "react-reveal/Fade";
 import "../styles/Skills.css";
 
 export default function Skills() {
@@ -21,11 +22,44 @@ export default function Skills() {
   ));
   return (
     <div className="skills">
-      <h1 className="skillsTitle">SKILLS</h1>
+      <Fade left duration={1200} delay={200}>
+        <h1 className="skillsTitle">SKILLS</h1>
+      </Fade>
       <div className="skillsRectangle">
-        {/* <div className="="> */}
-          {mappedSkills}
-          {/* </div> */}
+        <div className="skillsContainer">
+          <Fade left big duration={2000}>
+            <div className="technicalRectangle">
+              <h3>Technical</h3>
+            </div>
+          </Fade>
+          {/* <Fade top big>
+            <div className="singleSkill">React</div>
+            <div className="singleSkill">Javascript</div>
+            <div className="singleSkill">HTML</div>
+            <div className="singleSkill">CSS</div>
+            <div className="singleSkill">Ruby</div>
+            <div className="singleSkill">Rails</div>
+            <div className="singleSkill">PostgreSQL</div>
+            <div className="singleSkill">Figma</div>
+          </Fade> */}
+        </div>
+        <div className="skillsContainer">
+          <Fade right big duration={2000} delay={300}>
+            <div className="technicalRectangle">
+              <h3>Soft</h3>
+            </div>
+          </Fade>
+          {/* <Fade top big>
+            <div className="singleSkill">React</div>
+            <div className="singleSkill">Javascript</div>
+            <div className="singleSkill">HTML</div>
+            <div className="singleSkill">CSS</div>
+            <div className="singleSkill">Ruby</div>
+            <div className="singleSkill">Rails</div>
+            <div className="singleSkill">PostgreSQL</div>
+            <div className="singleSkill">Figma</div>
+          </Fade> */}
+        </div>
       </div>
     </div>
   );
