1da121897b414deee8b5780199189e7dd3ddb28f
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2021-02-11 23:11:47 +0100

    added two from four projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,10 +6,10 @@ export default function Projects() {
     <div className="projects">
       <h1 className="projectsTitle">PROJECTS</h1>
       <div className="grid-container">
-        <div style={{ backgroundColor: "purple" }}></div>
-        <div style={{ backgroundColor: "red" }}></div>
-        <div style={{ backgroundColor: "pink" }}></div>
-        <div style={{ backgroundColor: "orange" }}></div>
+        <div className="project" style={{ backgroundImage: 'url(images/portfolio.png'}}></div>
+        <div className="project" style={{ backgroundImage: 'url(images/shadr.png'}}></div>
+        <div className="project" style={{ backgroundColor: "pink" }}></div>
+        <div className="project" style={{ backgroundColor: "orange" }}></div>
       </div>
     </div>
   );
