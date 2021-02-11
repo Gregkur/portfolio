1ebd6ac9c406e727b951a01afa6660548817da22
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2021-02-11 23:44:15 +0100

    added 4 projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,10 +6,18 @@ export default function Projects() {
     <div className="projects">
       <h1 className="projectsTitle">PROJECTS</h1>
       <div className="grid-container">
-        <div className="project" style={{ backgroundImage: 'url(images/portfolio.png'}}></div>
-        <div className="project" style={{ backgroundImage: 'url(images/shadr.png'}}></div>
-        <div className="project" style={{ backgroundColor: "pink" }}></div>
-        <div className="project" style={{ backgroundColor: "orange" }}></div>
+        <div
+          className="project"
+          style={{ backgroundImage: "url(images/portfolio.png" }}></div>
+        <div
+          className="project"
+          style={{ backgroundImage: "url(images/shadr.png" }}></div>
+        <div
+          className="project"
+          style={{ backgroundImage: "url(images/social.png" }}></div>
+        <div
+          className="project"
+          style={{ backgroundImage: "url(images/art.png" }}></div>
       </div>
     </div>
   );
