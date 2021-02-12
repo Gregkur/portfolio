f968e0bf13b9b7ce5d9bb94f69aabffa2c8f6dbe
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2021-02-12 17:11:02 +0100

    changed buttons to fontawesome

--- src/components/ModalWrapper.js
+++ src/components/ModalWrapper.js
@@ -1,5 +1,9 @@
 import React from "react";
 import Zoom from "react-reveal/Zoom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faChrome } from "@fortawesome/free-brands-svg-icons";
 
 import "../styles/Modal.css";
 
@@ -15,11 +19,19 @@ const ModalWrapper = (props) => {
           <h1>{props.title}</h1>
         </header>
         <div className="buttonContainer">
-          <button className="buttonModal" onClick={props.hideModal}>
+          <FontAwesomeIcon
+            className="mIcon close"
+            icon={faTimes}
+            onClick={props.hideModal}
+          />
+
+          {/* <button className="buttonModal" onClick={props.hideModal}>
             Close
-          </button>
-          <button className="buttonModal">Github</button>
-          <button className="buttonModal">Demo</button>
+          </button> */}
+          <FontAwesomeIcon className="mIcon link" icon={faGithub} />
+          <FontAwesomeIcon className="mIcon link" icon={faChrome} />
+          {/* <button className="buttonModal">Github</button>
+          <button className="buttonModal">Demo</button> */}
         </div>
         {props.children}
       </div>
